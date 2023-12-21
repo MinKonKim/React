b753eae90e316cf9cfd143466ae637f29e7e9700
Minkon Kim <alsrhs98@gmail.com>
2023-12-21 10:21:02 +0900

    2023.12.20

--- expressjs/controllers/products.controller.js
+++ expressjs/controllers/products.controller.js
@@ -20,9 +20,18 @@ async function getProducts(req, res, next) {
 async function getProductsById(req, res, next) {
   try {
     const product = await productModel.findById(req.params.productId);
-  } catch (error) {}
+    if (product) {
+      res.status(200).json(product);
+    } else {
+      res.status(404).send();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 module.exports = {
   createProduct,
+  getProducts,
+  getProductsById,
 };
